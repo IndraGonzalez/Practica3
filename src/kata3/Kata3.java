595b9804e11d37e02890328960bfeb585718fbee
Indra <Indra@10.230.170.195>
2015-10-13 10:34:57 +0100

    Versión3

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,31 +1,26 @@
 
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Kata3 {
 
     public static void main(String[] args) {
         Histogram<String> histo = new Histogram<>();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
+        String pathName = "C://Users//Indra//Desktop//email.txt";
+
+        try{
+            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
+            String mail;
+            while((mail = fileIn.readLine()) != null){
+                if(!mail.contains("@")) continue;
+            }
+            histo.increment(mail.split("@")[1].toLowerCase());
+        } catch (Exception e) {
+            System.out.println("El fichero no existe");
+        }
         
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        
-   
         HistogramDisplay histoDisplay = new HistogramDisplay(histo);
         histoDisplay.execute();
     }
